9a3765624d4b3e0bc7207e203886868707a04f68
Jian Jin <jijin@ucdavis.edu>
2014-12-30 11:53:05 -0800

    add sim.rank.order function

---
+++ R/SimAnneal.R
@@ -0,0 +1,86 @@
+SimAnneal <- function(PMat, kmax=1000) {
+  
+  #PMat = ret
+  
+  ## Part I
+  N <- nrow(PMat)
+  ColSum <- sapply(1:N, function(n) sum(PMat[,n]))
+  Order <- order(ColSum)
+  C <- Cost(Reorder(PMat, Order))
+  C1 <- C
+  Cb <- C
+  Ordb <- Order
+  
+  ## Part II
+  k <- 0
+  l <- 1
+  while(k < kmax) {
+    NewOrder <- FindNeighbor(PMat,Order)
+    Cn <- Cost(Reorder(PMat, NewOrder))
+    if(Cn < C) {
+      C <- Cn
+      Order <- NewOrder
+      if(Cn < Cb) {
+        Cb <- Cn
+        Ordb <- NewOrder
+      }
+    } else
+      if( AP(C, Cn, Temp(k/kmax)) > runif(1) ) {
+        C <- Cn
+        Order <- NewOrder
+      }
+    l <- l+1
+    if((l %% 10) == 0) k <- k+1
+  }
+  return(list(C1=C1, Cb=Cb, Ordb=Ordb))
+}
+
+Cost <- function(Mat) {
+  N <- nrow(Mat)
+  Cost <- 0
+  for(i in 2:N) for(j in 1:(i-1)) Cost <- Cost + max(0, -log(2*(1-Mat[i,j]))) * 
+    exp((N+1-j)*(i-j)*2/N^2)
+  return(Cost)
+}
+
+# This is way faster than the old Reorder function!
+Reorder = function(Mat, Ord){
+  Mat[Ord, Ord]
+}
+
+
+FindNeighbor <- function(Mat, Ord) {
+  #Mat = ret
+  #Ord = Order
+  CMat <- Reorder(Mat, Ord)
+  N <- nrow(Mat)
+  Vals <- vector("numeric")
+  for(i in 1:(N-1))  Vals <- c(Vals, CMat[(i+1):N,i])
+  Q <- quantile(Vals, 0.8)
+  I <- vector("numeric")
+  J <- vector("numeric")
+  Pr <- vector("numeric")
+  for(i in 2:N) for(j in 1:(i-1)) if(CMat[i,j] >= Q) {
+    I <- c(I, i)
+    J <- c(J, j)
+    Pr <- c(Pr, CMat[i,j])
+  }
+  Pr <- Pr/sum(Pr)
+  S <- sample(x=1:length(Pr), size=1, prob=Pr)
+  Seq <- 1:N
+  Seq[c(I[S],J[S])] <- Seq[c(J[S],I[S])]
+  Ord <- Ord[Seq]
+  return(Ord)
+}
+
+
+
+
+AP <- function(e1, e2, T) {
+  if(e2 < e1) return(1)
+  return( exp((e1 - e2)/T/0.1) )
+}
+
+Temp <- function(num) {
+  return( 1 - exp((num-1)*5) )
+}
---
+++ R/sim.rank.order.R
@@ -0,0 +1,42 @@
+sim.rank.order <- function(df, kmax = 5){
+  # input df, a dataframe, the output from percolation function.
+  
+  # run the SimAnneal.R manually. 
+  percMat2 <- as.matrix(df) 
+  ### Run the simulated annealing many times, since it sometimes gets 
+  ### stuck in local minima.
+  sim.ann1 = SimAnneal(percMat2, kmax)
+  sim.ann2 = SimAnneal(percMat2, kmax)
+  sim.ann3 = SimAnneal(percMat2, kmax)
+  sim.ann4 = SimAnneal(percMat2, kmax)
+  sim.ann5 = SimAnneal(percMat2, kmax)
+  sim.ann6 = SimAnneal(percMat2, kmax)
+  sim.ann7 = SimAnneal(percMat2, kmax)
+  sim.ann8 = SimAnneal(percMat2, kmax)
+  sim.ann9 = SimAnneal(percMat2, kmax)
+  sim.ann10 = SimAnneal(percMat2, kmax)
+  
+  ## select the sim.ann with smallest output
+  sim.ann.list <- list(sim.ann1, sim.ann2, sim.ann3, sim.ann4, sim.ann5, 
+                       sim.ann6, sim.ann7, sim.ann8, sim.ann9, sim.ann10)
+  
+  sim.ann.all <- c(sim.ann1[[2]], sim.ann2[[2]], sim.ann3[[2]], sim.ann4[[2]],
+                   sim.ann5[[2]], sim.ann6[[2]], sim.ann7[[2]], sim.ann8[[2]],
+                   sim.ann9[[2]], sim.ann10[[2]])
+  
+  sim.ann.min <- min(sim.ann.all)
+  
+  Index.sim.ann <- which(sim.ann.all == sim.ann.min)
+  
+  ranking = sim.ann.list[[Index.sim.ann]]$Ordb
+  
+  RankingOrder <- data.frame(SubjectRanking = 1:length(ranking), 
+                             IDindex = ranking)
+  IDList.index <- data.frame(IDindex = 1:length(names(df)), 
+                             ID = sort(as.numeric(names(df))))
+  
+  RankingID <- merge(RankingOrder, IDList.index, by = "IDindex")
+  
+  Rank <- RankingID[,c("SubjectRanking", "ID")]
+  return(Rank)
+}
