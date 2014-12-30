9b25d7162a24f9b54d07e63ad9cc9237c7573dea
Jian Jin <jijin@ucdavis.edu>
2014-12-30 11:41:08 -0800

    attach sample data to the package and documenting the sample data

--- R/SampleEdgelist.R
+++ R/SampleEdgelist.R
@@ -1,10 +1,11 @@
-#' dominance interactions among 98 monkeys, with Initiator being the winner and Recipient being the loser.
-#'
-#' @format A data frame of edgelist with 53940 rows and 10 variables:
+#' SampleEdgelist.
+#' dominance interactions among 98 monkeys
+#' 
+#' @format A data frame of edgelist with 53940 rows and 2 variables: \code{Initiator}, \code{Recipient}
 #' \describe{
 #'   \item{Initiator}{winner, animal ID}
 #'   \item{Recipient}{loser, animal ID}
 #'   ...
 #' }
-#' @source {McCowan Lab sample data}
+#' McCowan Lab sample data.
 "SampleEdgelist"
