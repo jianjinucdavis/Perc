da8eff63d92a7b09bae29b34335088949a3ef761
Jian Jin <jijin@ucdavis.edu>
2015-02-19 16:10:26 -0800

    add Sample Raw Matrix/document

---
+++ R/SampleRawMatrix.R
@@ -0,0 +1,7 @@
+#' SampleRawMatrix.
+#' dominance interactions between 16 monkeys
+#' 
+#' @format A 16 x 16 dataframe representing number of times that a row wins over a column
+#' 
+#' McCowan Lab sample data.
+"SampleEdgelist"
