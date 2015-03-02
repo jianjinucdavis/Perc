36aed70e606218af6b87ad7fcff055295f646684
Jian Jin <jijin@ucdavis.edu>
2015-03-02 05:30:38 -0800

    find ID paths by real ID

--- R/IDpaths.R
+++ R/IDpaths.R
@@ -1,13 +1,15 @@
 ### Need the function IDpaths() to find all dominance paths.
 
-IDpaths = function(conf, i, len){
-  ### IDpaths: function that identifies all unique dominance paths of order (len - 1) beginning at subject i
+IDpaths = function(conf, ID, len){
+  ### IDpaths: identifies all unique dominance paths of order (len - 1) beginning at selected subject (given by ID)
   ### conf: N-by-N conflict matrix whose (i,j)th element is the number of times i defeated j
-  ### i: the subject at the beginning of each dominance path
+  ### ID: the subject at the beginning of each dominance path
   ### len: the length of the dominance paths to be identified (len = order + 1)
   
+  i = which(row.names(confmatrix) == as.character(ID))
+  
   if(sum(conf[i,] > 0) == 0) return(matrix(0, 0, len+1))
-  # i = 1                            ###? does i always == 1?
+  # i = 1                            
   #len = 5
   levels = list()
   levels[[1]] = which(conf[i,] > 0)  ###? levels[[1]]?
@@ -46,4 +48,3 @@ IDpaths = function(conf, i, len){
   isUnique = apply(ret, MARGIN = 1, function(b) {length(unique(b)) == len + 1})
   ret[isUnique,]    
 }
-
