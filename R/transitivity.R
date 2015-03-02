0a79e57ca469ebfeec08fc29432c53794fc05955
Jian Jin <jijin@ucdavis.edu>
2015-03-02 06:16:47 -0800

    transitivity test passed

---
+++ R/transitivity.R
@@ -0,0 +1,92 @@
+### transitivity() takes in a conflict matrix "conf" and outputs a list of 4 items
+#  1: The number of transitive triangles.
+#  2: The number of intransitive triangles.
+#  3: The order-1 transitivity.
+#  4: The value of alpha corresponding to this value of transitivity.
+###
+
+transitivity = function(conf){
+  
+  N = nrow(conf)
+  
+  
+  ### These lines set up the transitivity calculation.
+  ### We are making a matrix of all the possible sets of three subjects.
+  ### We won't really need to refer to this matrix.
+  ### The important part of the calculation is at the end.
+  
+  numrows = 0
+  ctr = 0
+  for(i in 1:(N-2)){
+    ctr = ctr + i
+    numrows = numrows + ctr
+  }
+  
+  firstrow = numeric(0)
+  
+  for(i in 1:(N-2)){
+    temp = rep(i, (N-1-i)*(N-i)/2)
+    firstrow = c(firstrow, temp)
+  }
+  
+  secondrow = numeric(0)
+  for(i in 1:(N-2)){ # first row number
+    for(j in (i+1):(N-1)){
+      temp = rep(j, N-j)
+      secondrow = c(secondrow, temp)
+    }
+  }
+  
+  thirdrow = numeric(0)
+  for(i in 3:N){
+    for(j in i:N){
+      thirdrow = c(thirdrow, seq(j, N, 1))
+    }
+  }
+  
+  
+  triples = matrix(0, numrows, 3)
+  triples[,1] = firstrow
+  triples[,2] = secondrow
+  triples[,3] = thirdrow
+  
+  
+  ## Here's where the actual transitivity calculation begins.
+  
+  transitive = 0
+  intransitive = 0
+  tList = matrix(0, 0, 4)
+  iList = matrix(0, 0, 4)
+  for(i in 1:nrow(triples)){
+    tA = triples[i,1]
+    tB = triples[i,2]
+    tC = triples[i,3]
+    AB = conf[tA, tB] - conf[tB, tA]
+    AC = conf[tA, tC] - conf[tC, tA]
+    BC = conf[tB, tC] - conf[tC, tB]
+    ### See if the triangle is transitive...
+    if((AC > 0 & BC > 0 & AB != 0) | 
+       (AB < 0 & AC < 0 & BC != 0) | 
+       (AB > 0 & BC < 0 & AC != 0)){
+      transitive = transitive + 1
+      tList = rbind(tList, c(triples[i,], i))
+    }
+    ### See if the triangle is intransitive...
+    if((AB > 0 & BC > 0 & AC < 0) | 
+       (AB < 0 & AC > 0 & BC < 0)){
+      intransitive = intransitive + 1
+      iList = rbind(iList, c(triples[i,], i))
+    }
+  }
+  
+  
+  ### Estimated transitivity is the proportion of transitive triangles.
+  T1 = transitive / (transitive + intransitive)
+  
+  ### T1 is the order-1 transitivity.
+  
+  ### From the paper, we estimate alpha as follows.
+  alpha = (2 * sqrt(T1) - 1) / (1 - sqrt(T1))
+  
+  return(list(transitive = transitive, intransitive = intransitive, transitivity = T1, alpha = alpha))
+}
