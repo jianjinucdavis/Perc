49e3c03fe15f02f23d3c2598047ae0fb7b54057b
Jian Jin <jijin@ucdavis.edu>
2015-03-02 05:56:42 -0800

    keep IDpaths original output

--- R/IDpaths.R
+++ R/IDpaths.R
@@ -46,8 +46,8 @@ IDpaths = function(conf, ID, len){
     prevLengths = effLengths
   }
   isUnique = apply(ret, MARGIN = 1, function(b) {length(unique(b)) == len + 1})
-  pathOutputmatrix <- ret[isUnique,]
-  for (i in 1:length(path.output)){
-    pathOutputmatrix[i] <- row.names(confmatrix)[path.output[i]]
-  }
+  #pathOutputmatrix <- ret[isUnique,]
+  #for (i in 1:length(path.output)){
+  #  pathOutputmatrix[i] <- row.names(confmatrix)[path.output[i]]
+  #}
 }
