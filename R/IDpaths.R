d104e546ebb46353baebbe736d4548d89c2b1d0c
Jian Jin <jijin@ucdavis.edu>
2015-03-05 09:39:08 -0800

    compare IDpaths.R

--- R/IDpaths.R
+++ R/IDpaths.R
@@ -1,15 +1,13 @@
 ### Need the function IDpaths() to find all dominance paths.
 
-IDpaths = function(conf, ID, len){
-  ### IDpaths: identifies all unique dominance paths of order (len - 1) beginning at selected subject (given by ID)
+IDpaths = function(conf, i, len){
+  ### IDpaths: function that identifies all unique dominance paths of order (len - 1) beginning at subject i
   ### conf: N-by-N conflict matrix whose (i,j)th element is the number of times i defeated j
-  ### ID: the subject at the beginning of each dominance path
+  ### i: the subject at the beginning of each dominance path
   ### len: the length of the dominance paths to be identified (len = order + 1)
   
-  i = which(row.names(conf) == as.character(ID))
-  
   if(sum(conf[i,] > 0) == 0) return(matrix(0, 0, len+1))
-  # i = 1                            
+  # i = 1                            ###? does i always == 1?
   #len = 5
   levels = list()
   levels[[1]] = which(conf[i,] > 0)  ###? levels[[1]]?
@@ -46,9 +44,5 @@ IDpaths = function(conf, ID, len){
     prevLengths = effLengths
   }
   isUnique = apply(ret, MARGIN = 1, function(b) {length(unique(b)) == len + 1})
-  ret[isUnique,]
-  #pathOutputmatrix <- ret[isUnique,]
-  #for (i in 1:length(path.output)){
-  #  pathOutputmatrix[i] <- row.names(confmatrix)[path.output[i]]
-  #}
+  ret[isUnique,]    
 }
