747ea25effac380578437c2044726cdfbf5e13b3
Jian Jin <jijin@ucdavis.edu>
2015-03-06 14:31:44 -0800

    clean up

--- R/findAllPaths.R
+++ R/findAllPaths.R
@@ -13,18 +13,6 @@
 #' allp.3 <- findAllPaths(confmatrix, 3)
 
 
-
-###############################################################################
-### Description: Identifies all paths length less than or equal to maxLength
-###              between all pairs of competitors.  Used in conductance.
-### Input:
-###   conf - N-by-N conflict matrix whose (i,j)th element is the number of 
-###          times i defeated j
-###   maxLength - a positive numeric integer indicating the maximum length
-###                of paths to identify
-### Output: list whose elements are all paths of a given length
-###############################################################################
-
 findAllPaths = function(conf, maxLength = 2){
   paths = lapply(2:maxLength, FUN = function(l, conf){
     do.call(rbind, lapply(1:nrow(conf), FUN = IDpaths, conf = conf, l = l))
