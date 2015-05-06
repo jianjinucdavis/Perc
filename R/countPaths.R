91c9501ce66b98642f24f25f217662da3496836f
Jian Jin <jijin@ucdavis.edu>
2015-05-06 15:46:50 -0700

    add names for countPaths output

--- R/countPaths.R
+++ R/countPaths.R
@@ -32,7 +32,8 @@
 
 
 countPaths = function(conf, maxLength = 2){
-  allPaths <- findAllPaths(conf, maxLength)
+#  allPaths <- findAllPaths(conf, maxLength)
+  allPaths <- allPaths(conf, maxLength)[[2]]
   nOrders = length(allPaths)
   pathList = list()
   N = max(sapply(allPaths, max))  #! sapply needs to be fixed. #! N was not used.
@@ -40,7 +41,15 @@ countPaths = function(conf, maxLength = 2){
   for(K in 1:nOrders){
     pathList[[K]] = as.conflictmat(allPaths[[K]][,c(1,(K+1))]) #! return error:"Error in a[[1]][, c(1, (1 + 1))] : incorrect number of dimensions"
   }
-  pathList
+   
+  cpNames  <- dimnames(confmatrix)[[1]][as.numeric(dimnames(pathList[[1]])[[1]])]
+  
+  for (i in 1:length(pathList)){
+    dimnames(pathList[[i]])[[1]] <- cpNames
+    dimnames(pathList[[i]])[[2]] <- cpNames
+  }
+  
+  return(pathList)
 }
 
 
