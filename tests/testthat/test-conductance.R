e876762e7182d3b110f626f113690f8c6b8c2a6b
Jian Jin <jijin@ucdavis.edu>
2015-07-09 11:54:49 -0700

    remove unuseful commented lines

--- tests/testthat/test-conductance.R
+++ tests/testthat/test-conductance.R
@@ -16,11 +16,7 @@ edgelist1 <- edgelist1[-which(edgelist1$col1 == edgelist1$col2), ]
 testMatrix2 <- as.conflictmat(edgelist1)
 
 test_that("input 'conf' is of 'conf.mat'", {
-  #set.seed(1)
-  #edgelist1 <- data.frame(col1 = sample(letters[1:26], 100, replace = TRUE), 
-  #                        col2 = sample(letters[1:26], 100, replace = TRUE), 
-  #                        stringsAsFactors = FALSE)
-  #edgelist1 <- edgelist1[-which(edgelist1$col1 == edgelist1$col2), ]
+  
   testMatrix1 <- edgelisttomatrix(edgelist1)
   expect_error(conductance(testMatrix1, maxLength = 2),
                "Turn conf into a 'conf.mat' using 'as.conflictmat'.")
@@ -28,24 +24,14 @@ test_that("input 'conf' is of 'conf.mat'", {
 
 
 test_that("return error for incorrect maxLength", {
-  #set.seed(1)
-  #edgelist1 <- data.frame(col1 = sample(letters[1:15], 200, replace = TRUE), 
-  #                        col2 = sample(letters[1:15], 200, replace = TRUE), 
-  #                        stringsAsFactors = FALSE)
-  #edgelist1 <- edgelist1[-which(edgelist1$col1 == edgelist1$col2), ]
-  # testMatrix2 <- as.conflictmat(edgelist1)
+  
   expect_error(conductance(testMatrix2, maxLength = 1), "'maxLength' should be an integer greater than 1 and less than 7.")
   expect_error(conductance(testMatrix2, maxLength = 2.3), "'maxLength' needs to be an integer.")
   
 })
 
 test_that("output is a list of length 2", {
-  #set.seed(1)
-  #edgelist1 <- data.frame(col1 = sample(letters[1:15], 200, replace = TRUE), 
-  #                        col2 = sample(letters[1:15], 200, replace = TRUE), 
-  #                        stringsAsFactors = FALSE)
-  #edgelist1 <- edgelist1[-which(edgelist1$col1 == edgelist1$col2), ]
-  #testMatrix2 <- as.conflictmat(edgelist1)
+  
   expect_is(conductance(testMatrix2, maxLength = 2), "list")
   expect_equal(length(conductance(testMatrix2, maxLength = 2)), 2)
 })
@@ -53,14 +39,7 @@ test_that("output is a list of length 2", {
 
 
 test_that("outputs are correct", {
-  #set.seed(1)
-  #edgelist1 <- data.frame(col1 = sample(letters[1:26], 100, replace = TRUE), 
-  #                        col2 = sample(letters[1:26], 100, replace = TRUE), 
-  #                        stringsAsFactors = FALSE)
-  #edgelist1 <- edgelist1[-which(edgelist1$col1 == edgelist1$col2), ]
-  #testMatrix1 <- as.conflictmat(edgelist1)
-  #   edgelist1 <- data.frame(x = letters[1:5], y = letters[6:2])
-  #testMatrix2 <- as.conflictmat(edgelist1)
+ 
   expect_equal_to_reference(conductance(testMatrix2, maxLength = 2), file = "conductanceOutput1.rds")
   expect_equal_to_reference(conductance(testMatrix2, maxLength = 3), file = "conductanceOutput2.rds")
   expect_equal_to_reference(conductance(testMatrix2, maxLength = 4), file = "conductanceOutput3.rds")
