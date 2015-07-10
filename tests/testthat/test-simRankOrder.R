1a92fd84130760fe01d406ebbf8bd61c40e239f0
Jian Jin <jijin@ucdavis.edu>
2015-07-10 15:19:12 -0700

    output tests for sim

--- tests/testthat/test-simRankOrder.R
+++ tests/testthat/test-simRankOrder.R
@@ -13,6 +13,7 @@ edgelist1 <- data.frame(col1 = sample(letters[1:15], 200, replace = TRUE),
                         stringsAsFactors = FALSE)
 edgelist1 <- edgelist1[-which(edgelist1$col1 == edgelist1$col2), ]
 testMatrix2 <- as.conflictmat(edgelist1)
+conductanceOutput <- conductance(testMatrix2, 2)
 
 # tests
 test_that("input 'data' is of 'matrix'", {
@@ -22,7 +23,6 @@ test_that("input 'data' is of 'matrix'", {
 })
 
 test_that("values in input 'data' is between 0 - 1.", {
-  conductanceOutput <- conductance(testMatrix2, 2)
   conductanceOutput[[2]][1,5] <- -1
   expect_error(simRankOrder(conductanceOutput[[2]], num = 2, kmax = 5),
                "The second element 'p.hat' from the output of 'conductance' should be used.")
@@ -30,5 +30,17 @@ test_that("values in input 'data' is between 0 - 1.", {
   conductanceOutput[[2]][1,5] <- 1.5
   expect_error(simRankOrder(conductanceOutput[[2]], num = 2, kmax = 5),
                "The second element 'p.hat' from the output of 'conductance' should be used.")
+  
+})
+
+
+test_that("output is a list of length 3", {
+  simOutput <- simRankOrder(conductanceOutput[[2]], num = 2, kmax = 5)
+  expect_output(str(simOutput), "List of 3")
+})
+
+test_that("output is correct", {
+  simOutput <- simRankOrder(conductanceOutput[[2]], num = 2, kmax = 5)
+  expect_equal_to_reference(simOutput, file = "simOutput1.rds")
   
 })
